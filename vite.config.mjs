4da07a405e0fa980ecb126ddd7cc68619e0056c3
komish_Re <14369410+komishre@user.noreply.gitee.com>
2026-02-25 22:08:52 +0800

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite' // 必须加上这一行
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/'  // 在 Vercel 部署建议用 '/'
+  base: '/' 
 })
